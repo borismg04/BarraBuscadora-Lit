8a4e23b5e77673e4d2bad1e97276e023b2f5f0bb
bmonroyg <bmonroyg@emeal.nttdata.com>
2022-08-24 14:29:44 -0500

    initial

--- src/AppBarrabuscadora.js
+++ src/AppBarrabuscadora.js
@@ -1,7 +1,4 @@
 import { LitElement, html, css } from 'lit';
-
-const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
-
 export class AppBarrabuscadora extends LitElement {
   static get properties() {
     return {
@@ -11,52 +8,8 @@ export class AppBarrabuscadora extends LitElement {
 
   static get styles() {
     return css`
-      :host {
-        min-height: 100vh;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: flex-start;
-        font-size: calc(10px + 2vmin);
-        color: #1a2b42;
-        max-width: 960px;
-        margin: 0 auto;
-        text-align: center;
-        background-color: var(--app-barrabuscadora-background-color);
-      }
 
-      main {
-        flex-grow: 1;
-      }
-
-      .logo {
-        margin-top: 36px;
-        animation: app-logo-spin infinite 20s linear;
-      }
-
-      @keyframes app-logo-spin {
-        from {
-          transform: rotate(0deg);
-        }
-        to {
-          transform: rotate(360deg);
-        }
-      }
-
-      .app-footer {
-        font-size: calc(12px + 0.5vmin);
-        align-items: center;
-      }
-
-      .app-footer a {
-        margin-left: 5px;
-      }
     `;
-  }
-
-  constructor() {
-    super();
-    this.title = 'My app';
   }
 
   render() {
